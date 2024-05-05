ee0014c10f817c5ad38352d5b5ac458895f5f7fc
emeksdot <emeksdot@yahoo.com>
2024-05-05 05:23:57 +0100

    Revert "mistake"
    This reverts commit 3eb4f1420b1f580d3d6c3488aba6e8617a0b19a1.

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,6 +3,3 @@ const bit = year.getFullYear();
 console.log(bit);
 
 document.querySelector("#lastModified").innerHTML = bit;
-
-const nav = document.querySelector("nav");
-
--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector("#hamburger");
-const nav1 = document.querySelector("nav");
-
-hamburger.addEventListener("click", () => {
-  nav1.children.classList.toggle("show");
-  //   nav1.classList.toggle("show");
-  hamburger.classList.toggle("show");
-});
