fcfdf36eae15d1afb82ddf511061ad020ce1ce8e
emeksdot <emeksdot@yahoo.com>
2024-05-16 23:56:37 +0100

    started work on the chamber homepage

---
+++ chamber/scripts/script.js
@@ -0,0 +1,15 @@
+const today = new Date();
+const bit = today.getFullYear();
+
+document.querySelector(".year").innerHTML = bit;
+
+const dateModified = new Date(document.lastModified);
+document.querySelector("#lastModified").innerHTML = dateModified;
+
+const hamburger = document.querySelector("#menu");
+const nav = document.querySelector(".navigation");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("show");
+  nav.classList.toggle("show");
+});
