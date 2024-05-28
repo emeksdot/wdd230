528899d24b1ee576bced68778101c45a376238a1
emeksdot <emeksdot@yahoo.com>
2024-05-28 09:13:11 +0100

    replaced the rangevalue div to a span

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -51,3 +51,24 @@ numVisits++;
 // 5️⃣ store the new visit total into localStorage, key=numVisits-ls
 localStorage.setItem("numberOfVisits", numVisits);
 
+const password = document.querySelector("#password");
+const confirmPassword = document.querySelector("#confirmPassword");
+
+confirmPassword.addEventListener("focusout", () => {
+  if (password.value !== confirmPassword.value) {
+    confirmPassword.classList.add("check");
+  } else {
+    confirmPassword.classList.remove("check");
+    confirmPassword.setAttribute(
+      "borderRight",
+      "6px solid var(--required-color)"
+    );
+  }
+});
+
+const rating = document.querySelector("#rating");
+const rangevalue = document.querySelector(".rangevalue");
+
+rating.addEventListener("input", () => {
+  rangevalue.innerHTML = rating.value;
+});
