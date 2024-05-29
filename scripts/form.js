dedbe9af85a49f2051c32a7f6b74c37680427ac1
emeksdot <emeksdot@yahoo.com>
2024-05-29 01:08:46 +0100

    corrected the input

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,11 @@
+const today = new Date();
+const bit = today.getFullYear();
+
+document.querySelector(".year").innerHTML = bit;
+
+const dateModified = new Date(document.lastModified);
+document.querySelector("#lastModified").innerHTML = dateModified;
+
 const password = document.querySelector("#password");
 const confirmPassword = document.querySelector("#confirmPassword");
 
