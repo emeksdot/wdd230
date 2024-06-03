947037ea425b50de90522e8f3ff1ee53664e3fc1
emeksdot <emeksdot@yahoo.com>
2024-06-03 14:35:02 +0100

    fixed the links.json file

--- scripts/links.js
+++ scripts/links.js
@@ -2,13 +2,14 @@ const baseURL = "https://emeksdot.github.io/wdd230/";
 
 const linksURL = "https://emeksdot.github.io/wdd230/data/links.json";
 
+
 async function linksFetch() {
   try {
     const response = await fetch(linksURL);
     if (response.ok) {
       const data = await response.json();
       console.log(data);
-      //   displayResults(data);
+    //   displayLinks(data);
     } else {
       throw Error(await response.text());
     }
@@ -16,5 +17,24 @@ async function linksFetch() {
     console.log(error);
   }
 }
+
+// const displayLinks = (weeks) => {
+  // for (let index = 0; index < weeks.length; index++) {
+  //   const element = array[index];
+  // }
+//   weeks.array.forEach((item) => {
+//     const li = document.createElement("li");
+//     const txt = document.createTextNode(item.links[0].title);
+//     const a = document.createElement("a");
+//     a.appendChild(txt);
+//     li.appendChild(a);
+//     const ul = document.createElement("ul");
+//     const card = document.querySelector("#card");
+//     card.appendChild(ul)
+
+
+//     ul.appendChild(li);
+//   });
+// };
 
 linksFetch();
