8ec8a35b0563c629481cc2c3422997445c933da4
emeksdot <emeksdot@yahoo.com>
2024-06-04 19:55:47 +0100

    started the table on the directory page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -19,3 +19,45 @@ let lastVisit = document.querySelector(".lastVisit");
 let message = document.querySelector(".message");
 
 
+// Directory Code
+const baseURL = "https://emeksdot.github.io/wdd230/";
+
+const linksURL = "https://emeksdot.github.io/wdd230/chamber/data/members.json";
+
+async function directoryFetch() {
+  try {
+    const response = await fetch(linksURL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+          console.log(data.companies);
+          console.log(data.companies[0]);
+          console.log(data.companies[0].names);
+
+      buildTable(data.companies);
+
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+const buildTable = (dat) => {
+  const table = document.querySelector(".myTable");
+  for (let index = 0; index < dat.length; index++) {
+    // console.log(data[index].names);
+    let row = ` <tr>
+      <td>${dat[index].names}</td>
+      <td>${dat[index].address}</td>
+      <td>${dat[index].phoneNumber}</td>
+      <td>${dat[index].website}</td>
+    </tr>`;
+    table.innerHTML += row;
+  }
+};
+
+
+directoryFetch();
--- scripts/getDates.js
+++ scripts/getDates.js
@@ -102,3 +102,23 @@ const rangevalue = document.querySelector(".rangevalue");
 rating.addEventListener("input", () => {
   rangevalue.innerHTML = rating.value;
 });
+
+
+const baseURL = "https://emeksdot.github.io/wdd230/";
+
+const linksURL = "https://emeksdot.github.io/wdd230/data/links.json";
+
+async function linksFetch() {
+  try {
+    const response = await fetch(linksURL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      //   displayLinks(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
